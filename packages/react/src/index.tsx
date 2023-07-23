ac0d1f4a5dee79c355c224c4243b13081ceef48a
steniomoreira <steniomoreiradev@gmail.com>
2023-07-23 00:00:56 -0300

    feat: added stitches react

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,12 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$10',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Simab is my dog</h1>
+  return <Button>Simab is my dog</Button>
 }
